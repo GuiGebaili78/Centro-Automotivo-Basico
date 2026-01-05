fd93baaedebc82eb0f97feed80b69c7e61d4e042
GuiGebaili78 <103890957+GuiGebaili78@users.noreply.github.com>
2026-01-05 16:41:51 -0300

    Correção do vazamento de conexões causando lentidão

--- server/src/repositories/contasPagar.repository.ts
+++ server/src/repositories/contasPagar.repository.ts
@@ -1,7 +1,5 @@
 
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '../prisma.js';
 
 export class ContasPagarRepository {
     async create(data: any) {
